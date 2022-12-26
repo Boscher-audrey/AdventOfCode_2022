521355752dfc100695418d158b23187416782416
Audrey Boscher <a.boscher@myunisoft.fr>
2022-12-26 18:28:26 +0100

    response day 2 - part 2

--- Day2/index.js
+++ Day2/index.js
@@ -43,9 +43,56 @@ fs.readFile('./input', 'utf8', function(err, data) {
 
     const result = roundPoints(movePlayer1, movePlayer2)
     totalRoundScore += result
-})
+  })
 
-console.log('Step 1 - Total round score : ', totalRoundScore)
+  console.log('Step 1 - Total round score : ', totalRoundScore)
 
+  const topSecretRoundMove = (movePlayer1, movePlayer2) => {
+    // Player needs to loose
+    if (movePlayer2 === 'X') {
+      switch(movePlayer1) {
+        case 'A':
+          return 'Z'
+        case 'B':
+          return 'X'
+        case 'C':
+          return 'Y'
+      }
+      // Player needs to win
+    } else if (movePlayer2 === 'Z') {
+      switch(movePlayer1) {
+        case 'A':
+          return 'Y'
+        case 'B':
+          return 'Z'
+        case 'C':
+          return 'X'
+      }
+      // Player needs to end the round in a draw
+    } else if (movePlayer2 === 'Y') {
+      switch(movePlayer1) {
+        case 'A':
+          return 'X'
+        case 'B':
+          return 'Y'
+        case 'C':
+          return 'Z'
+      }
+    }
+  }
+
+  let totalTopSecretRoundScore = 0
+
+  allRounds.forEach((round) => {
+    const movesPlayers = round.split(' ')
+    const movePlayer1 = movesPlayers[0]
+    const movePlayer2 = movesPlayers[1]
+
+    const newMovePlayer2 = topSecretRoundMove(movePlayer1, movePlayer2)
+    const result = roundPoints(movePlayer1, newMovePlayer2)
+    totalTopSecretRoundScore += result
+  })
+
+  console.log('Step 2 - Total top secret round score : ', totalTopSecretRoundScore)
 });
 
