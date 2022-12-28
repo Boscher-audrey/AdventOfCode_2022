dd27c15ac4f98f8362d7ec6b3cddf6f4605b403e
Audrey Boscher <a.boscher@myunisoft.fr>
2022-12-28 17:11:27 +0100

    wip day 4

--- Day4/index.js
+++ Day4/index.js
@@ -1,3 +1,29 @@
 import fs from 'fs';
 
-fs.readFile('./input', 'utf8', function (err, data) {});
+fs.readFile('./input', 'utf8', function (err, data) {
+  const pairs = data.split(`\r\n`);
+
+  const range = (start, stop) => Array.from({ length: stop - start + 1 }, (_, i) => start + i);
+
+  let pairsThatMatches = 0;
+
+  pairs.map((pair) => {
+    const elvesRange = pair.split(',');
+
+    let arrayNumbers = [];
+
+    elvesRange.forEach((elfRange) => {
+      const rangeNumbers = elfRange.split('-');
+      arrayNumbers.push(range(parseInt(rangeNumbers[0]), parseInt(rangeNumbers[1])));
+    });
+
+    const joinNumbersElfOne = arrayNumbers[0].join(',');
+    const joinNumbersElfTwo = arrayNumbers[1].join(',');
+
+    if (joinNumbersElfOne.includes(joinNumbersElfTwo) || joinNumbersElfTwo.includes(joinNumbersElfOne)) {
+      pairsThatMatches++;
+    }
+  });
+
+  console.log('Step 1 - Number of assignment pairs that matches the range : ', pairsThatMatches);
+});
