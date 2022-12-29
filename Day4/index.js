58b04d97034d62b6260b4704905b5a48840848d5
Audrey Boscher <a.boscher@myunisoft.fr>
2022-12-29 11:03:06 +0100

    response day 4 - part 2

--- Day4/index.js
+++ Day4/index.js
@@ -6,6 +6,7 @@ fs.readFile('./input', 'utf8', function (err, data) {
   const range = (start, stop) => Array.from({ length: stop - start + 1 }, (_, i) => start + i);
 
   let pairsThatMatches = 0;
+  let pairsThatOverlap = 0;
 
   pairs.map((pair) => {
     const elvesRange = pair.split(',');
@@ -28,13 +29,36 @@ fs.readFile('./input', 'utf8', function (err, data) {
       });
     };
 
-    const rangeNumbersElfOne = formatNumbers(0).toString();
-    const rangeNumbersElfTwo = formatNumbers(1).toString();
+    const rangeNumbersElfOne = formatNumbers(0);
+    const rangeNumbersElfTwo = formatNumbers(1);
 
-    if (rangeNumbersElfOne.includes(rangeNumbersElfTwo) || rangeNumbersElfTwo.includes(rangeNumbersElfOne)) {
+    const rangeNumbersElfOneString = rangeNumbersElfOne.toString();
+    const rangeNumbersElfTwoString = rangeNumbersElfTwo.toString();
+
+    if (
+      rangeNumbersElfOneString.includes(rangeNumbersElfTwoString) ||
+      rangeNumbersElfTwoString.includes(rangeNumbersElfOneString)
+    ) {
       pairsThatMatches++;
+    }
+
+    let endFor = 0;
+
+    for (let i = 0; i < rangeNumbersElfOne.length; i++) {
+      if (endFor !== 0) {
+        break;
+      }
+
+      for (let j = 0; j < rangeNumbersElfTwo.length; j++) {
+        if (rangeNumbersElfOne[i] === rangeNumbersElfTwo[j]) {
+          pairsThatOverlap++;
+          endFor++;
+          break;
+        }
+      }
     }
   });
 
   console.log('Step 1 - Number of assignment pairs that matches the range : ', pairsThatMatches);
+  console.log('Step 2 - Number of assignment pairs that ranges overlap : ', pairsThatOverlap);
 });
